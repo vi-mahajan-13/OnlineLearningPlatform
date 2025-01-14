18272e87e236f096b17c64f2cb2aedd0d720ff95
Vicki Mahajan <vimahajan@bestpeers.in>
2025-01-14 11:45:12 +0530

    Add progress tag and refactor views with partials.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,6 +6,8 @@ class Ability
     if user.admin?
       can :manage, :all
     elsif user.instructor?
+      can :create, Course
+      can :create, Lesson, course: { user_id: user.id } 
       can :manage, Course, user_id: user.id
       can :manage, Lesson, course: { user_id: user.id }
       can :read, Course, user_id: user.id
